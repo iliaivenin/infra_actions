6cb28f5466033466bea5fbb280655c28f5382b60
Илья Ивенин <ivenin_i@mail.ru>
2021-09-13 18:54:40 +0300

    edit workflow main.yml

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!2')
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
